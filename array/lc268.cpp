4c8b6183890334f72e9d277c64b121ae01752c8b
Lalitforreal <s24cseu0093@bennett.edu.in>
2025-10-09 16:27:29 +0530

    missing number

---
+++ array/lc268.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n+1);
+        for(int i = 0; i<n; i++){
+            freq[nums[i]]++;
+        }
+        for(int i = 0; i<n; i++){
+            if(freq[i] == 0){
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
