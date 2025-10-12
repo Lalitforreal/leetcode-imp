10bc76528760fdbc556ce66d0f22a45cbb36b75b
Lalitforreal <s24cseu0093@bennett.edu.in>
2025-10-12 18:59:19 +0530

    merge list

---
+++ linked-list/mergeList.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* left = list1;
+        ListNode* right = list2;
+        //new LL
+        ListNode* dummy = new ListNode(0); //head of new ll
+        ListNode* curr = dummy;
+
+        
+        
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                curr->next = list1;
+                list1 = list1->next;
+            }else{
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr =  curr->next;
+        }
+
+        //handle leftovers
+        if(list1){
+            curr->next = list1;
+        }else{
+            curr->next = list2;
+        }
+
+        return dummy->next;
+    }
+};
